86607b17ccaf9cb62b7492db3627f20ce8655e32
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2026-02-24 16:16:05 +0900

    Title: 최대공약수 구하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260224/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 260224/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -3,16 +3,14 @@ n, m = map(int, input().split())
 # Please write your code here.
 def GCD(n,m):
     max = 0
+
     if n > m:
         min = m
     else:
         min = n
-    
-    if(min == 1):
-        max = 1
-    
-    for i in range(1, min):
-        if(n % i == 0 & m & i == 0):
+
+    for i in range(1, min+1):
+        if(n % i == 0 and m % i == 0):
             max = i
     
     print(max)

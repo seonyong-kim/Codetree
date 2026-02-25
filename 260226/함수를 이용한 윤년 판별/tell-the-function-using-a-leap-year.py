835aa80e4ff42f187a9baf39b09bbb1ae1887ee9
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2026-02-26 00:26:20 +0900

    Title: 함수를 이용한 윤년 판별, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260226/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,10 @@
+y = int(input())
+
+# Please write your code here.
+if(y%4 == 0):
+    if(y%100 == 0 and y%400 != 0):
+        print("false")
+    else:
+        print("true")
+else:
+    print("false")

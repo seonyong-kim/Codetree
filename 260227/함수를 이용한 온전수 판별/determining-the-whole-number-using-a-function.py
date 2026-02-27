70acba3414efcc5da693a80aabc36b53077c566e
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2026-02-27 22:05:19 +0900

    Title: 함수를 이용한 온전수 판별, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260227/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def checkOn(a,b):
+    count = 0
+    for i in range(a,b):
+        if(i % 2 != 0 and i%10 != 5):
+            if (i%3 == 0 and i%9 != 0):
+                continue
+            count += 1
+    return count
+
+
+print(checkOn(a,b))

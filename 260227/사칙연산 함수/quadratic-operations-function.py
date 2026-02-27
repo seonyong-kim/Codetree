382f5806605e5adf303dc208748060b43b492d7f
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2026-02-27 20:31:43 +0900

    Title: 사칙연산 함수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260227/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,21 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+def cal(a, o, c):
+    if (o == '+'):
+        return a+c
+    elif (o == '-'):
+        return a-c
+    elif (o=='/'):
+        return a/c
+    elif (o == '*'):
+        return a*c
+    else:
+        return "False"
+
+if(cal(a,o,c) != "False"):
+    print(f"{a} {o} {c} = ",cal(a,o,c), sep="")
+else:
+    print(cal(a,o,c))

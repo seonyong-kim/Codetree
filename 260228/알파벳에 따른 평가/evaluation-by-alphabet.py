513d46d1d98b499c84afb15dbc095627ff1f38db
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2026-02-28 13:38:40 +0900

    Title: 알파벳에 따른 평가, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260228/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+str = input()
+if str == "S":
+    print("Superior")
+elif str == "A":
+    print("Excellent")
+elif str == "B":
+    print("Good")
+elif str == "C":
+    print("Usually")
+elif str == "D":
+    print("Effort")
+else:
+    print("Failure")

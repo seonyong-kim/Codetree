1a6289de978e7facca59577c274a88c6d2d09473
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2026-03-05 20:22:54 +0900

    Title: 4가지 관계연산자, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260305/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,22 @@
+arr = input().split()
+A,B = int(arr[0]), int(arr[1])
+
+if(A >= B):
+    print(1)
+else:
+    print(0)
+
+if(A > B):
+    print(1)
+else:
+    print(0)
+
+if(B >= A):
+    print(1)
+else:
+    print(0)
+
+if(B > A):
+    print(1)
+else:
+    print(0)
